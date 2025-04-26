e508dfb302d46b5edd84c66d8eb9d7ddffd9948b
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2025-04-26 18:35:54 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,10 +7,6 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-// var _ = net.Listen
-// var _ = os.Exit
-
 func main() {
 	fmt.Println("Starting the server at port 4221.")
 
@@ -44,27 +40,44 @@ func main() {
 	if len(lines) > 0 {
 		firstHeader := lines[0]
 		fmt.Println(firstHeader)
-		res := strings.Split(firstHeader, " ")
+		res := strings.Fields(firstHeader)
 		path := res[1]
-		fmt.Println(path)
+		fmt.Println("path: ", path)
+		// for idx, line := range lines {
+		// 	fmt.Printf("line %d: \n--------------------\n%s\n--------------------\n", idx, line)
+		// }
+
+		// Handle root path
 		if path == "/" {
 			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		} else if strings.Contains(path, "echo") {
+
 			responseData := strings.Split(path, "/")[2]
 			response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(responseData), responseData)
-			if err != nil {
-				fmt.Println("Error formatting data.", err.Error())
-				os.Exit(1)
-			}
 			conn.Write([]byte(response))
+
+		} else if strings.Contains(path, "user-agent") {
+
+			var userAgent string
+			for _, line := range lines {
+				if strings.HasPrefix(line, "User-Agent:") {
+
+					userAgent = strings.TrimSpace(strings.TrimPrefix(line, "User-Agent:"))
+					break
+				}
+			}
+
+			if userAgent != "" {
+				response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+				conn.Write([]byte(response))
+			} else {
+				conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			}
+
 		} else {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 		}
-
 	}
 
-	// conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-
-	// fmt.Println("No data sent. Closing connection.")
-	// conn.Close()
+	conn.Close()
 }
